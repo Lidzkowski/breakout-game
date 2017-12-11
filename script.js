0ffb0706c51915e2cada32407e6da02bb01a4b22
Lukas <luuq82@gmail.com>
2017-12-11 16:53:46 +0100

    play with basic canvas.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('myCanvas'); // ste the variable for vanvas
+var ctx = canvas.getContext('2d'); //create canvas objects in 2d
 
+ctx.beginPath(); //draw a square
+ctx.rect(20, 40, 100, 100);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();  //circle
+ctx.arc(140, 190, 50, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+ 
+ctx.beginPath(); // rectangle
+ctx.rect(160, 80, 200, 40);
+ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+ctx.stroke();
+ctx.beginPath();
+
+ctx.moveTo(480,0); //line
+ctx.lineTo(0,320);
+ctx.stroke();
